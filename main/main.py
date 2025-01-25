2c980f213af2ccdded17c9d5d435bd195a8c1ba4
Keys4Ever <cubillagonzalo@outlook.com>
2025-01-25 16:11:41 -0300

    refactor: Improve input functions and update print statements for clarity

--- main/main.py
+++ main/main.py
@@ -6,38 +6,45 @@ import time
 PORT = 8787
 BUFFER_SIZE = 1024
 
-PEER_IP = "192.168.1.2" 
+PEER_IP = ""
 PEER_PORT = PORT
 IsRecieved = False
 
-def peer_ip_input():
+# Function to input the peer ip and port
+def peer_input():
     global PEER_IP
-    PEER_IP = input("Ingrese la IP del otro dispositivo: ")
-    print(f"IP del otro dispositivo: {PEER_IP}")
+    global PEER_PORT
+    PEER_IP = input("Other device ip: ")
+    print(f"Other device ip: {PEER_IP}")
+    PEER_PORT = int(input("Input the other device port (default 8787): ") or PORT)
 
+# Function to input the local port
+def local_input():
+    global PORT
+    PORT = int(input("Input the local port (default 8787): ") or PORT)
 
-# Función para ejecutar el servidor y recibir datos
+# Function to receive the clipboard data from the other device
 def server_thread(host="0.0.0.0", port=PORT):
     global IsRecieved
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind((host, port))
     server_socket.listen(1)
-    print(f"Servidor escuchando en {host}:{port}...")
+    print(f"Server listening {host}:{port}...")
 
     while True:
         conn, addr = server_socket.accept()
-        print(f"Conexión entrante desde {addr}")
+        print(f"Connection from {addr}")
 
         data = conn.recv(BUFFER_SIZE).decode('utf-8')
         if data:
-            print(f"Recibido: {data}")
+            print(f"Received: {data}")
             pyperclip.copy(data)
             IsRecieved = True
-            print("Clipboard actualizado!")
+            print("Clipboard updated!")
 
         conn.close()
 
-# Función para enviar datos al otro dispositivo
+# Function to send the clipboard data to the other device
 def client_thread(peer_ip, peer_port):
     global IsRecieved
     last_clipboard = pyperclip.paste()
@@ -52,15 +59,16 @@ def client_thread(peer_ip, peer_port):
                 client_socket.connect((peer_ip, peer_port))
                 client_socket.sendall(current_clipboard.encode('utf-8'))
                 client_socket.close()
-                print(f"Enviado: {current_clipboard}")
+                print(f"Sended: {current_clipboard}")
             except Exception as e:
-                print(f"Error al enviar: {e}")
+                print(f"Error while sending: {e}")
         elif IsRecieved == True:
             last_clipboard = current_clipboard
             IsRecieved = False
 
-# Inicia el servidor y el cliente en threads separados
+# Starts the server and the client in separeted threads
 if __name__ == "__main__":
-    peer_ip_input()
+    peer_input()
+    local_input()
     threading.Thread(target=server_thread, daemon=True).start()
     client_thread(PEER_IP, PEER_PORT)
